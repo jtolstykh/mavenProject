6a6343fa754c5a714c3dcae3a9531ca431ea3151
katalina <ekaterinmax1mova@yandex.ru>
2022-05-14 14:10:58 +0300

    Initial commit

---
+++ src/main/java/Country.java
@@ -0,0 +1,20 @@
+public class Country {
+
+    String name;
+    long population;
+    String capital;
+    String formOfPower;
+    double area;
+
+    public Country(String name, long population, String capital, String formOfPower, double area) {
+        this.name = name;
+        this.population = population;
+        this.capital = capital;
+        this.formOfPower = formOfPower;
+        this.area = area;
+    }
+
+    public Country() {
+    }
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import com.google.gson.Gson;
+
+public class Main {
+    Gson gson= new Gson();
+    Country russia = new Country("russia", 147000000L, "Moscow","democracy",17100000L);
+
+
+}
